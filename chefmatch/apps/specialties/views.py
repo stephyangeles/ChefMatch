4e9d819f446b9105d9dee48966ef46a770402880
a-bac-0 <anca.bacria@thevallians.com>
2025-03-03 02:35:51 +0100

    crud_create: add chef serializer

--- chefmatch/apps/specialties/views.py
+++ chefmatch/apps/specialties/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Specialty
-from .serializers import SpecialtySerializer
+from .models import Specialty, Chef
+from .serializers import SpecialtySerializer, ChefSerializer
 
 @api_view(['GET', 'POST'])
 def item_list(request):
