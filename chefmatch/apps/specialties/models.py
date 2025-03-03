dac07f4b339955ac37e23706ed8ec78b1add9e81
a-bac-0 <anca.bacria@thevallians.com>
2025-03-03 02:38:47 +0100

    crud_create: add chef model

--- chefmatch/apps/specialties/models.py
+++ chefmatch/apps/specialties/models.py
@@ -8,3 +8,14 @@ class Specialty(models.Model):
     
     class Meta:
         db_table = 'specialty'
+
+class Chef(models.Model):
+    name = models.CharField(max_length=100)
+    expertise = models.CharField(max_length=100)
+    rating = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'chef'
