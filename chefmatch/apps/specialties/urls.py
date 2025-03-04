fc8a1cbad0ed962272df97c14bc74366eb79918f
a-bac-0 <anca.bacria@thevallians.com>
2025-03-04 02:26:29 +0100

    feature: add reservation view and url

--- chefmatch/apps/specialties/urls.py
+++ chefmatch/apps/specialties/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SpecialtyViewSet, ChefViewSet, UserViewSet
+from .views import SpecialtyViewSet, ChefViewSet, UserViewSet, ReservationViewSet
 
 router = DefaultRouter()
 router.register(r'specialties', SpecialtyViewSet)
 router.register(r'chefs', ChefViewSet)
 router.register(r'users', UserViewSet)
+router.register(r'reservations', ReservationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
