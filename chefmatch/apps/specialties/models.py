9f17a3fad64dd0aae3ba778c213faeef7ea328b0
a-bac-0 <anca.bacria@thevallians.com>
2025-03-04 01:51:48 +0100

    feature: add users model

--- chefmatch/apps/specialties/models.py
+++ chefmatch/apps/specialties/models.py
@@ -19,3 +19,14 @@ class Chef(models.Model):
     
     class Meta:
         db_table = 'chef'
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telephone = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'users'
