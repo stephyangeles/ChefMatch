29e395885ea19eca3288f93d41400ed20ff449cf
Stephyangeles <stephanydelosangeles.lizarraga@tajamar365.com>
2025-03-05 15:13:21 +0100

    feature: fix settings, urls, views

--- chefmatch/apps/reservations/admin.py
+++ chefmatch/apps/reservations/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Reservation
 
-admin.site.register(Reservation)
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'chef', 'date', 'location', 'confirmed_at', 'canceled_at', 'completed_at')
