ae81d0bcd7fdc7010a0f8f9e6133bbbb3721078a
Camila <milarodcar@gmail.com>
2025-03-05 14:32:29 +0100

    Fix reservation table creation SQL and changed chefs for chef

--- db/tables/reservations.sql
+++ db/tables/reservations.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS reservations (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    chefs_id INT NOT NULL,
+    chef_id INT NOT NULL,
     date TIMESTAMP NOT NULL,
     location VARCHAR(255) NOT NULL,
     confirmed_at TIMESTAMP,
     canceled_at TIMESTAMP,
     completed_at TIMESTAMP,
-    chefs_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (chefs_id) REFERENCES chefs(id) ON DELETE CASCADE
+    FOREIGN KEY (chef_id) REFERENCES chef(id) ON DELETE CASCADE
 );
