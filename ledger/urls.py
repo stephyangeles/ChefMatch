6343547f3cb3a8273225f252ff67806a9d6aee2e
milarodcar <74417105+milarodcar@users.noreply.github.com>
2025-03-09 17:48:17 +0100

    Delete ledger directory

--- ledger/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ledger/', views.ledger_list, name='ledger_list'),
-]
--- ledger/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import GeneralLedger
-
-def ledger_list(request):
-    ledger_entries = GeneralLedger.objects.all()
-    return render(request, 'ledger_list.html', {'ledger_entries': ledger_entries})
