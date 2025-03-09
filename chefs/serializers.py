d0e739941326199494eab6c913319b74bf52d9f3
milarodcar <74417105+milarodcar@users.noreply.github.com>
2025-03-09 17:48:57 +0100

    Delete chefs directory

--- chefs/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Chef
-
-class ChefSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Chef
-        fields = '__all__'
--- chefs/views.py
+++
@@ -1,7 +0,0 @@
-from .models import Chef
-from rest_framework import viewsets
-from .serializers import ChefSerializer
-
-class ChefViewSet(viewsets.ModelViewSet):
-    queryset = Chef.objects.all()
-    serializer_class = ChefSerializer
